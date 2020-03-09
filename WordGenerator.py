8f664783f36a78b1bf58c2d001f57e3a1aafe854
Jack Turpitt <jackturpitt@gmail.com>
2020-03-09 21:07:25 +1300

    Refactor the WordGenerator
    Refactored the WordGenerator by breaking some of the larger
    functions into several smaller functions. This should help
    to improve code readability.

--- WordGenerator.py
+++ WordGenerator.py
@@ -3,68 +3,80 @@ import re
 import sys
 
 
+def default_viability_func(generated_word):
+    min_length = 3
+    max_length = 10
+    max_consonant_string = 3
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+
+    unviable = False
+
+    # Ensure there are vowels.
+    unviable = unviable or not any(char in vowels for char in generated_word)
+
+    # Ensure the name is long enough but not too long.
+    unviable = unviable or len(generated_word) > max_length
+    unviable = unviable or len(generated_word) < min_length
+
+    # Ensure there are never too many consonants in a row.
+    consonant_strings = re.findall(r'[^aeiou]+', generated_word)
+    unviable = unviable or not consonant_strings
+    unviable = unviable or len(max(consonant_strings, key=len)) > max_consonant_string
+
+    return not unviable
+
+
 class WordGenerator:
-    class ProbabilityElement:
+    class MarkovElement:
         def __init__(self):
             self.chars = []
-            self.probs = []
+            self.probabilities = []
 
-    @staticmethod
-    def _is_viable_name(name):
-        min_length = 3
-        max_length = 10
-        max_consonant_string = 3
-        vowels = ['a', 'e', 'i', 'o', 'u', 'y']
-
-        unviable = False
-
-        # Ensure there are vowels.
-        unviable = unviable or not any(char in vowels for char in name)
-
-        # Ensure the name is long enough but not too long.
-        unviable = unviable or len(name) > max_length
-        unviable = unviable or len(name) < min_length
-
-        # Ensure there are never too many consonants in a row.
-        consonant_strings = re.findall(r'[^aeiou]+', name)
-        unviable = unviable or not consonant_strings
-        unviable = unviable or len(max(consonant_strings, key=len)) > max_consonant_string
-
-        return not unviable
-
-    def __init__(self, input_path):
+    def __init__(self, input_path, viability_func=default_viability_func):
         self.input_path = input_path
-
-        # Load the probability table into a usable data type.
-        self.probs = {}
-        with open(self.input_path) as input_file:
-            for line in input_file:
-                tokens = line.rstrip().split(' ')
-                prev_char = tokens[0][0]
-                curr_char = tokens[0][1]
-                prob = tokens[1]
-
-                if prev_char not in self.probs:
-                    self.probs[prev_char] = WordGenerator.ProbabilityElement()
-                self.probs[prev_char].chars.append(curr_char)
-                self.probs[prev_char].probs.append(float(prob))
+        self.word_is_viable = viability_func
+        self._load_table()
 
     def generate_name(self):
-        # Generate names until we are within the length range.
         name = ''
-        while not self._is_viable_name(name):
-            name = '^'
-            while name[-1] != '$':
-                name += self._get_curr_char(name[-1])
-            name = name[:-1][1:]
-
+        while not self.word_is_viable(name):
+            name = self._generate_raw_name()
         name = name.capitalize()
         return name
 
-    def _get_curr_char(self, prev_char):
-        # Get the next character based on the given probabilities.
-        char = numpy.random.choice(self.probs[prev_char].chars, p=self.probs[prev_char].probs)
+    def _generate_raw_name(self):
+        name = '^'
+        while name[-1] != '$':
+            prev_char = name[-1]
+            name += self._choose_curr_char(prev_char)
+        stripped_name = self._strip_special_chars_from(name)
+        return stripped_name
+
+    def _choose_curr_char(self, prev_char):
+        ''' Get the next character based on the markov probabilities of the current character. '''
+        markov_elem = self.markov_elems[prev_char]
+        char = numpy.random.choice(markov_elem.chars, p=markov_elem.probabilities)
         return char
+
+    def _load_table(self):
+        self.markov_elems = {}
+        with open(self.input_path) as input_file:
+            for line in input_file:
+                prev_char, curr_char, prob = self._parse_table_line(line)
+
+                if prev_char not in self.markov_elems:
+                    self.markov_elems[prev_char] = WordGenerator.MarkovElement()
+                self.markov_elems[prev_char].chars.append(curr_char)
+                self.markov_elems[prev_char].probabilities.append(float(prob))
+
+    @staticmethod
+    def _parse_table_line(line):
+        tokens = line.rstrip().split(' ')
+        return tokens[0][0], tokens[0][1], tokens[1]
+
+    @staticmethod
+    def _strip_special_chars_from(word):
+        return word[:-1][1:]
 
 
 if __name__ == '__main__':
@@ -77,5 +89,5 @@ if __name__ == '__main__':
     input_path = sys.argv[1]
 
     generator = WordGenerator(input_path)
-    for _ in range(10):
+    for _ in range(30):
         print(generator.generate_name())
